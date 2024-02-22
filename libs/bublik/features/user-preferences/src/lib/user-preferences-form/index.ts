f23ea3088960f3d27356a2f609dca4865019d592
Danil Kostromin <danil.kostromin@arknetworks.am>
2024-02-22 19:49:37 +0200

    fix(preferences): missing hook export
    Missing hook export for getting user preferences
    
    Signed-off-by: Danil Kostromin <danil.kostromin@arknetworks.am>

--- libs/bublik/features/user-preferences/src/lib/user-preferences-form/index.ts
+++ libs/bublik/features/user-preferences/src/lib/user-preferences-form/index.ts
@@ -1,3 +1,4 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2024 OKTET LTD */
 export * from './user-preferences-form.container';
+export * from './user-preferences.hooks';
