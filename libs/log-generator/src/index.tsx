3e0bde8c3c57988a9c0aa076e647e123e6a0cde9
Danil Kostromin <danil.kostromin@arknetworks.am>
2024-03-18 03:07:16 +0200

    Generate initial html
    Signed-off-by: Danil Kostromin <danil.kostromin@arknetworks.am>

--- libs/log-generator/src/index.tsx
+++ libs/log-generator/src/index.tsx
@@ -1,10 +1,48 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 import { Command } from 'commander';
 import { renderToString } from 'react-dom/server';
+import postcss from 'postcss';
+import { PropsWithChildren } from 'react';
 
-function ExampleApp() {
-	return <div>Hello world</div>;
+const tailwind = require('tailwindcss');
+const fs = require('fs');
+
+interface GenerateCSSConfig {
+	html: string;
+}
+
+async function getCss({ html }: GenerateCSSConfig) {
+	const tailwindConfig = { content: [], theme: { extend: {} }, plugins: [] };
+	const sourceCSS = '@tailwind base; @tailwind components; @tailwind utilities';
+
+	const config = {
+		presets: [tailwindConfig],
+		content: [{ raw: html }]
+	};
+
+	return postcss([tailwind(config)]).process(sourceCSS);
+}
+
+function App() {
+	return <div className="bg-red-500">Hello world</div>;
+}
+
+function Root(props: PropsWithChildren<{ css: string }>) {
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<title>Log Page</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<style>{props.css}</style>
+			</head>
+			<body className="overflow-hidden antialiased font-medium selection:text-white selection:bg-primary text-text-primary bg-bg-body">
+				<div id="root">{props.children}</div>
+			</body>
+		</html>
+	);
 }
 
 const program = new Command();
@@ -14,8 +52,18 @@ program
 	.description('Generate HTML log from JSON');
 
 program.addCommand(
-	new Command('generate').action(() => {
-		console.log(renderToString(<ExampleApp />));
+	new Command('generate').action(async () => {
+		const html = renderToString(<App />);
+		const css = await getCss({ html });
+		const fullApp = renderToString(
+			<Root css={css.toString()}>
+				<App />
+			</Root>
+		);
+
+		await fs.writeFile('log.html', fullApp, (err: unknown) => {
+			console.log(err);
+		});
 	})
 );
 
