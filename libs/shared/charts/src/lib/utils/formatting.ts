9d94e70a9ae371bba8f569b9b6bd0963be209f6e
Danil Kostromin <danil.kostromin@icloud.com>
2024-11-08 02:27:43 +0300

    refactor(measurements): adjusted getting chart name
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/shared/charts/src/lib/utils/formatting.ts
+++ libs/shared/charts/src/lib/utils/formatting.ts
@@ -1,12 +1,12 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
-import { MeasurementPlot } from '@/shared/types';
+import { SingleMeasurementChart } from '@/services/bublik-api';
 import { upperCaseFirstLetter } from '@/shared/utils';
 
 export const formatLabel = (label: string) => {
 	return label.split('_').map(upperCaseFirstLetter).join(' ');
 };
 
-export const getChartName = (plot: MeasurementPlot): string => {
-	return `${plot.type} - ${plot.axises_config.title}`;
+export const getChartName = (plot: SingleMeasurementChart): string => {
+	return plot.title ?? plot.subtitle;
 };
