3b8df42ec891fab6b79d03753b94268c3cee6085
Danil Kostromin <danil.kostromin@icloud.com>
2024-11-07 14:45:29 +0300

    feat(measurements): added endpoint and types for single measurement plot
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/services/bublik-api/src/lib/endpoints/measurements-endpoints.ts
+++ libs/services/bublik-api/src/lib/endpoints/measurements-endpoints.ts
@@ -2,36 +2,11 @@
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
 import { EndpointBuilder } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
 
-import {
-	ResultInfoAPIResponse,
-	MeasurementPlot,
-	MeasurementPlotWithoutId
-} from '@/shared/types';
+import { ResultInfoAPIResponse } from '@/shared/types';
 
 import { BUBLIK_TAG } from '../types';
 import { BublikBaseQueryFn, withApiV2 } from '../config';
 import { API_REDUCER_PATH } from '../constants';
-
-const createChartId = (measurement: MeasurementPlotWithoutId, idx: number) => {
-	return `${idx}_${measurement.tool}:${measurement.type}:${measurement.name}:${measurement.aggr}`;
-};
-
-const addIdToPlot = (
-	plot: MeasurementPlotWithoutId,
-	idx: number
-): MeasurementPlot => {
-	return { id: createChartId(plot, idx), ...plot };
-};
-
-const transformToMeasurementPlot = (
-	plotsWithoutIds: MeasurementPlotWithoutId[]
-) => {
-	if (!Array.isArray(plotsWithoutIds)) return { plots: [] };
-
-	const plots = plotsWithoutIds.map(addIdToPlot);
-
-	return { plots } as const;
-};
 
 export const measurementsEndpoints = {
 	endpoints: (
@@ -40,28 +15,47 @@ export const measurementsEndpoints = {
 		getResultInfo: build.query<ResultInfoAPIResponse, string | number>({
 			query: (resultId) => withApiV2(`/results/${resultId}`)
 		}),
-		getSingleMeasurement: build.query<MeasurementPlot[], string | number>({
+		getSingleMeasurement: build.query<SingleMeasurement, string | number>({
 			query: (resultId) => ({
-				url: withApiV2('/measurements/by_results_ids'),
-				method: 'POST',
-				body: { results_ids: [Number(resultId)] }
-			}),
-			transformResponse: (plotsWithoutId: MeasurementPlotWithoutId[]) => {
-				if (!Array.isArray(plotsWithoutId)) return [];
-
-				return plotsWithoutId.map(addIdToPlot);
-			}
+				url: withApiV2(`/results/${resultId}/measurements`)
+			})
 		}),
-		getMeasurements: build.query<
-			{ plots: MeasurementPlot[] },
-			(string | number)[]
-		>({
-			query: (results) => ({
-				url: withApiV2('/measurements/by_results_ids'),
-				method: 'POST',
-				body: { results_ids: results.map(Number) }
-			}),
-			transformResponse: transformToMeasurementPlot
-		})
+		getMeasurements: build.query<SingleMeasurementChart[], (string | number)[]>(
+			{
+				query: (results) => ({
+					url: withApiV2('/measurements/by_result_ids'),
+					method: 'POST',
+					body: { result_ids: results.map(Number) }
+				})
+			}
+		)
 	})
 };
+
+export interface SingleMeasurement {
+	run_id: number;
+	iteration_id: number;
+	charts: SingleMeasurementChart[];
+	tables: SingleMeasurementTable[];
+}
+
+export interface SingleMeasurementChart {
+	id: number;
+	title: string;
+	subtitle: string;
+	axis_x: { key: string; label: string };
+	axis_y: { key: string; label: string };
+	dataset: (string | number)[][];
+}
+
+export interface SingleMeasurementTable {
+	measurement_id: number;
+	type: string;
+	name: string;
+	tool: string;
+	aggr: string;
+	units: string;
+	keys: string[];
+	comments: string[];
+	value: number;
+}
