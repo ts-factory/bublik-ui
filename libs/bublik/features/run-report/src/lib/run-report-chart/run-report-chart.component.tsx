840d9f9a90859f7c50ac2aaa0d515931a1c56507
Danil Kostromin <danil.kostromin@icloud.com>
2024-11-26 05:04:22 +0300

    feat(report): [chart] open point info dialog when clicking on point
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/run-report/src/lib/run-report-chart/run-report-chart.component.tsx
+++ libs/bublik/features/run-report/src/lib/run-report-chart/run-report-chart.component.tsx
@@ -1,9 +1,15 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2024 OKTET LTD */
-import { ComponentProps, useMemo } from 'react';
+import { ComponentProps, useEffect, useMemo, useRef, useState } from 'react';
+import { z } from 'zod';
+import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
+import ReactEChartsCore from 'echarts-for-react/lib/core';
 
 import { Plot, chartStyles } from '@/shared/charts';
 import { ReportChart } from '@/shared/types';
+
+import { RunReportPointDialog } from '../run-report-point-dialog';
 
 interface RunReportChartProps {
 	chart: ReportChart;
@@ -11,6 +17,9 @@ interface RunReportChartProps {
 
 function RunReportChart(props: RunReportChartProps) {
 	const { chart } = props;
+	const { runId } = useParams<{ runId: string }>();
+	const [resultId, setResultId] = useState<number>();
+	const [open, setOpen] = useState(false);
 
 	const series = useMemo<
 		ComponentProps<typeof Plot>['options']['series']
@@ -22,40 +31,89 @@ function RunReportChart(props: RunReportChartProps) {
 		}));
 	}, []);
 
+	const chartRef = useRef<ReactEChartsCore>(null);
+
+	useEffect(() => {
+		const instance = chartRef.current?.getEchartsInstance();
+		if (!instance) return;
+
+		function handleClick(rawParams: unknown) {
+			const ParamsSchema = z
+				.object({
+					seriesIndex: z.number(),
+					dataIndex: z.number()
+				})
+				.nonstrict();
+
+			const paramsResult = ParamsSchema.safeParse(rawParams);
+			if (!paramsResult.success) {
+				toast.error('Failed to parse point data!');
+				return;
+			}
+
+			const params = paramsResult.data;
+
+			const meta =
+				chart.data[params.seriesIndex].points[params.dataIndex].metadata;
+
+			if (!meta?.result_id) {
+				toast.error('No result id found in metadata for point!');
+				return;
+			}
+
+			setResultId(meta?.result_id);
+			setOpen(true);
+		}
+
+		instance.on('click', handleClick);
+		return () => {
+			instance.off('click', handleClick);
+		};
+	}, []);
+
 	return (
-		<div className="w-full flex flex-col gap-2">
-			<Plot
-				options={{
-					toolbox: { top: 9999, feature: { dataZoom: {} } },
-					tooltip: {
-						trigger: 'axis',
-						textStyle: chartStyles.text,
-						extraCssText: 'shadow-popover rounded-lg',
-						axisPointer: { type: 'cross' }
-					},
-					legend: { data: chart.data.map((s) => s.series) },
-					grid: { containLabel: true },
-					dataZoom: [{}, { type: 'inside' }],
-					xAxis: {
-						type: 'category',
-						name: chart.axis_x.label,
-						nameLocation: 'middle',
-						nameGap: 20,
-						nameTextStyle: chartStyles.text,
-						axisLabel: { ...chartStyles.text }
-					},
-					yAxis: {
-						type: 'value',
-						name: chart.axis_y.label,
-						nameGap: 20,
-						nameLocation: 'end',
-						axisLabel: chartStyles.text,
-						nameTextStyle: chartStyles.text
-					},
-					series: series
-				}}
+		<>
+			<RunReportPointDialog
+				runId={Number(runId)}
+				resultId={resultId}
+				open={open}
+				onOpenChange={setOpen}
 			/>
-		</div>
+			<div className="w-full flex flex-col gap-2">
+				<Plot
+					ref={chartRef}
+					options={{
+						toolbox: { top: 9999, feature: { dataZoom: {} } },
+						tooltip: {
+							trigger: 'axis',
+							textStyle: chartStyles.text,
+							extraCssText: 'shadow-popover rounded-lg',
+							axisPointer: { type: 'shadow' }
+						},
+						legend: { data: chart.data.map((s) => s.series) },
+						grid: { containLabel: true },
+						dataZoom: [{}, { type: 'inside' }],
+						xAxis: {
+							type: 'category',
+							name: chart.axis_x.label,
+							nameLocation: 'middle',
+							nameGap: 20,
+							nameTextStyle: chartStyles.text,
+							axisLabel: { ...chartStyles.text }
+						},
+						yAxis: {
+							type: 'value',
+							name: chart.axis_y.label,
+							nameGap: 20,
+							nameLocation: 'end',
+							axisLabel: chartStyles.text,
+							nameTextStyle: chartStyles.text
+						},
+						series: series
+					}}
+				/>
+			</div>
+		</>
 	);
 }
 
