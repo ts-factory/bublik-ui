bd65b92df0a6eefd25a64b204fb6271f7969bbf9
Danil Kostromin <danil.kostromin@icloud.com>
2024-11-28 05:48:47 +0300

    feat(ui): [chart] allowed to override default options for chart
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/shared/charts/src/lib/plot/v2/plot.component.tsx
+++ libs/shared/charts/src/lib/plot/v2/plot.component.tsx
@@ -1,12 +1,13 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
-import { CSSProperties, forwardRef } from 'react';
+import { ComponentProps, CSSProperties, forwardRef } from 'react';
 import ReactEChartsCore from 'echarts-for-react/lib/core';
 
 import { EChartsOption, ReactECharts } from '../../echart';
 import { twTheme } from '../../tw-theme';
 
-export interface PlotProps {
+export interface PlotProps
+	extends Omit<ComponentProps<typeof ReactECharts>, 'option'> {
 	/* Options provided for plot */
 	options: EChartsOption;
 	/* Style for plot container */
@@ -17,7 +18,7 @@ export interface PlotProps {
 
 /* Internal plot that accepts all options use it as primitive for other charts */
 export const Plot = forwardRef<ReactEChartsCore, PlotProps>((props, ref) => {
-	const { options, className, style } = props;
+	const { options, className, style, ...restProps } = props;
 
 	return (
 		<ReactECharts
@@ -26,6 +27,7 @@ export const Plot = forwardRef<ReactEChartsCore, PlotProps>((props, ref) => {
 			className={className}
 			style={style}
 			notMerge={true}
+			{...restProps}
 			ref={ref}
 		/>
 	);
