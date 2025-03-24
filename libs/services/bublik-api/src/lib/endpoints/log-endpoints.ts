0565219a8e5f4b3226755196b2d7edfdf830808e
Danil Kostromin <danil.kostromin@icloud.com>
2025-03-24 11:30:15 +0300

    feat(log): add endpoint to fetch `artifacts.json`
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/services/bublik-api/src/lib/endpoints/log-endpoints.ts
+++ libs/services/bublik-api/src/lib/endpoints/log-endpoints.ts
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
+import { z } from 'zod';
+import { zodToJsonSchema } from 'zod-to-json-schema';
 import { EndpointBuilder } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
 import { QueryReturnValue } from '@reduxjs/toolkit/dist/query/baseQueryTypes';
 import { createNextState } from '@reduxjs/toolkit';
@@ -23,6 +25,78 @@ import {
 } from '../error-handling';
 import { ResultsAndVerdictsForIteration } from './run-endpoints';
 
+const RawLogArtifact = z.object({
+	type: z.literal('text').describe('type of artifact'),
+	view_type: z
+		.literal('inline')
+		.describe('inline means open in new browser tab'),
+	name: z.string().describe('name of artifact'),
+	description: z
+		.string()
+		.optional()
+		.describe('optional description of artifact'),
+	download_enabled: z
+		.boolean()
+		.optional()
+		.default(false)
+		.describe('should show button to download artifact'),
+	path: z
+		.string()
+		.optional()
+		.describe('path of artifact relative to artifacts.json'),
+	uri: z
+		.string()
+		.optional()
+		.describe(
+			'uri of artifact, if it is not relative to artifacts.json or external'
+		)
+});
+
+const LogArtifact = z.discriminatedUnion('type', [RawLogArtifact]);
+
+export type LogArtifactType = z.infer<typeof LogArtifact>['type'];
+
+export type GetLogArtifactByType<T extends LogArtifactType> = z.infer<
+	typeof LogArtifact
+> extends { type: T }
+	? z.infer<typeof LogArtifact>
+	: never;
+
+const ArtifactsSchema = z
+	.object({
+		version: z.number().describe('version of artifacts.json'),
+		artifacts: z.array(LogArtifact).describe('list of artifacts')
+	})
+	.describe('artifacts.json schema');
+
+type ArtifactsJsonResponse = {
+	artifact_base_url: string;
+	data: z.infer<typeof ArtifactsSchema>;
+};
+
+const ArtifactsSchemaJson = zodToJsonSchema(ArtifactsSchema);
+console.log(ArtifactsSchemaJson);
+
+function getArtifactBaseUrl(artifactsUrl: string): string {
+	try {
+		const url = new URL(artifactsUrl);
+
+		if (url.pathname.includes('/proxy/')) {
+			const originalUrl = new URL(url.searchParams.get('url') || '');
+			return originalUrl.href.replace(/\/artifacts\.json$/, '');
+		}
+
+		return artifactsUrl.replace(/\/artifacts\.json$/, '');
+	} catch (e) {
+		return artifactsUrl;
+	}
+}
+
+type LogUrlResponse = {
+	url: string;
+	artifacts_url: string;
+};
+
 export const constructJsonUrl = (input: GetLogJsonInputs): string => {
 	const PREFIX = '/api/v2';
 
@@ -33,6 +107,20 @@ export const constructJsonUrl = (input: GetLogJsonInputs): string => {
 	if (input.page) result += `/?page=${input.page}`;
 
 	return result;
+};
+
+const fetchJson = async (externalUrl: string) => {
+	const getBublikFromStatusCode = (response: Response): BublikHttpError => {
+		return { status: response.status };
+	};
+
+	const options: RequestInit = { credentials: 'include', cache: 'no-store' };
+
+	const response = await fetch(externalUrl, options);
+
+	if (!response.ok) throw getBublikFromStatusCode(response);
+
+	return response.json();
 };
 
 export const logEndpoints = {
@@ -50,28 +138,67 @@ export const logEndpoints = {
 			HistoryDefaultResultAPIResponse,
 			number | string
 		>({ query: (resultId) => withApiV2(`/results/${resultId}`) }),
-		getLogJson: build.query<RootBlock, GetLogJsonInputs>({
-			queryFn: async ({ page, id }, _api, _extraOptions, baseQuery) => {
-				const fetchJson = async (externalUrl: string) => {
-					const getBublikFromStatusCode = (
-						response: Response
-					): BublikHttpError => {
-						return { status: response.status };
+		getLogArtifacts: build.query<ArtifactsJsonResponse, number>({
+			queryFn: async (id, _api, _extraOptions, baseQuery) => {
+				try {
+					const getUrl = constructJsonUrl({ id });
+
+					const jsonUrl = (await baseQuery(
+						getUrl
+					)) as QueryReturnValue<LogUrlResponse>;
+
+					if (jsonUrl.error) {
+						return {
+							error: {
+								status: 400,
+								title: 'Something went wrong',
+								description:
+									'We are not sure what exactly went wrong, please report'
+							}
+						};
+					}
+
+					if (!jsonUrl.data) {
+						return {
+							error: {
+								status: 404,
+								title: 'No data',
+								description: 'No json data found in response'
+							}
+						};
+					}
+
+					const artifactsJson = await fetchJson(jsonUrl.data.artifacts_url);
+
+					return {
+						data: {
+							artifact_base_url: getArtifactBaseUrl(jsonUrl.data.artifacts_url),
+							data: artifactsJson
+						}
+					};
+				} catch (e) {
+					console.error(e);
+
+					const defaultError: BublikError = {
+						status: 400,
+						title: 'Something went wrong',
+						description:
+							'We are not sure what exactly went wrong, please report'
 					};
 
-					const options: RequestInit = { credentials: 'include' };
+					if (isBublikParsableError(e)) return { error: e };
 
-					const response = await fetch(externalUrl, options);
-
-					if (!response.ok) throw getBublikFromStatusCode(response);
-
-					return response.json();
-				};
+					return { error: defaultError };
+				}
+			}
+		}),
+		getLogJson: build.query<RootBlock, GetLogJsonInputs>({
+			queryFn: async ({ page, id }, _api, _extraOptions, baseQuery) => {
 				const getUrl = constructJsonUrl({ page, id });
 
-				const jsonUrl = (await baseQuery(getUrl)) as QueryReturnValue<{
-					url: string;
-				}>;
+				const jsonUrl = (await baseQuery(
+					getUrl
+				)) as QueryReturnValue<LogUrlResponse>;
 
 				if (jsonUrl.error) throw jsonUrl.error;
 
@@ -92,6 +219,7 @@ export const logEndpoints = {
 
 					const blocksWithAddedVerdictsAndArtifacts = addArtifactsVerdicts(
 						blocksJson,
+						// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 						artifactsAndVerdicts.data!
 					);
 
