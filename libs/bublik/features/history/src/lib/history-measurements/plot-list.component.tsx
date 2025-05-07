fd7c11507e9094604176b2c1b6d57ad57cb78718
Danil Kostromin <danil.kostromin@icloud.com>
2025-05-07 04:33:50 +0300

    refactor(history): [measurements] use selection state popover from shared lib
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/history/src/lib/history-measurements/plot-list.component.tsx
+++ libs/bublik/features/history/src/lib/history-measurements/plot-list.component.tsx
@@ -1,31 +1,28 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
-import { useMemo, useState } from 'react';
-import { Link, To, useSearchParams } from 'react-router-dom';
-import { AnimatePresence, motion, Transition, Variants } from 'framer-motion';
+import { useState } from 'react';
 
 import { Point } from '@/shared/types';
 import {
-	ButtonTw,
 	CardHeader,
 	cn,
 	Icon,
 	Skeleton,
 	ToolbarButton,
-	Tooltip,
 	useSidebar
 } from '@/shared/tailwind-ui';
 import {
 	ExportChart,
-	getChartName,
 	getColorByIdx,
-	MeasurementChart
+	isDisabledForCombined,
+	MeasurementChart,
+	SelectedChartsPopover
 } from '@/shared/charts';
 import { SingleMeasurementChart } from '@/services/bublik-api';
 import { LogPreviewContainer } from '@/bublik/features/log-preview-drawer';
 
 import { useCombinedView } from './plot-list.hooks';
-import { isDisabledForCombined, resolvePoint } from './plot-list.utils';
+import { resolvePoint } from './plot-list.utils';
 
 interface PlotListItemProps {
 	idx: number;
@@ -116,7 +113,8 @@ export function PlotList({ plots, isFetching }: PlotListProps) {
 		handleAddChartClick,
 		handleRemoveClick,
 		handleResetButtonClick,
-		selectedCharts
+		selectedCharts,
+		handleOpenButtonClick
 	} = useCombinedView();
 
 	return (
@@ -166,109 +164,14 @@ export function PlotList({ plots, isFetching }: PlotListProps) {
 					);
 				})}
 			</ul>
-			<PlotStackedForm
+			<SelectedChartsPopover
 				open={!!selectedCharts.length}
 				label="Combined"
 				plots={selectedCharts}
 				onResetButtonClick={handleResetButtonClick}
 				onRemoveClick={handleRemoveClick}
+				onOpenButtonClick={handleOpenButtonClick}
 			/>
 		</div>
 	);
 }
-
-const variants: Variants = {
-	visible: { opacity: 1, y: '0%' },
-	hidden: { opacity: 0, y: '100%' },
-	exit: { opacity: 0, y: '100%' }
-};
-
-const transition: Transition = { bounce: 0.1 };
-
-export interface PlotStackedFormProps {
-	label: string;
-	open: boolean;
-	plots: { plot: SingleMeasurementChart; color: string }[];
-	onResetButtonClick?: () => void;
-	onRemoveClick?: (plot: SingleMeasurementChart) => void;
-}
-
-const PlotStackedForm = (props: PlotStackedFormProps) => {
-	const [searchParams] = useSearchParams();
-
-	const linkToCombined = useMemo<To>(() => {
-		const params = new URLSearchParams(searchParams);
-
-		params.set('mode', 'measurements-combined');
-		params.set('combinedPlots', props.plots.map((p) => p.plot.id).join(';'));
-
-		return { pathname: '/history', search: params.toString() };
-	}, [props.plots, searchParams]);
-
-	return (
-		<AnimatePresence>
-			{props.open ? (
-				<motion.div
-					variants={variants}
-					initial="hidden"
-					animate="visible"
-					exit="exit"
-					transition={transition}
-					className="fixed bottom-4 right-4 bg-white rounded-lg shadow-popover min-w-[360px] px-4 py-2"
-				>
-					<h1 className="text-[0.875rem] leading-[1.125rem] font-semibold mb-2">
-						{props.label}
-					</h1>
-					<ul className="flex flex-col gap-2 mb-4">
-						{props.plots.map(({ plot }) => {
-							return (
-								<li
-									key={plot.id}
-									className="flex items-center justify-between p-2 border rounded border-border-primary hover:bg-gray-50"
-								>
-									<div>
-										<span className="text-[0.6875rem] font-medium leading-[0.875rem]">
-											{getChartName(plot)}
-										</span>
-									</div>
-									<Tooltip
-										content="Remove from combined chart"
-										disableHoverableContent
-									>
-										<button
-											aria-label="Remove from compare"
-											className="grid transition-colors rounded place-items-center text-text-unexpected hover:bg-red-100"
-											onClick={() => props.onRemoveClick?.(plot)}
-										>
-											<Icon name="CrossSimple" />
-										</button>
-									</Tooltip>
-								</li>
-							);
-						})}
-					</ul>
-					<div className="flex items-center gap-2">
-						<ButtonTw
-							variant="primary"
-							size="md"
-							rounded="lg"
-							className="flex-1"
-							asChild
-						>
-							<Link to={linkToCombined}>Open</Link>
-						</ButtonTw>
-						<ButtonTw
-							variant="outline"
-							size="md"
-							rounded="lg"
-							className="flex-1"
-							onClick={props.onResetButtonClick}
-						>
-							Reset
-						</ButtonTw>
-					</div>
-				</motion.div>
-			) : null}
-		</AnimatePresence>
-	);
-};
