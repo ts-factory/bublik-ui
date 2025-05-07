0d29de320d457b42467f5c78b1174570a83140da
Danil Kostromin <danil.kostromin@icloud.com>
2025-05-07 04:34:19 +0300

    feat(result): [measurements] add selection popover for combined charts
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/measurements/src/lib/components/mode-picker/index.tsx
+++ libs/bublik/features/measurements/src/lib/components/mode-picker/index.tsx
@@ -1,30 +1,66 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
-import { MeasurementsMode } from '@/shared/types';
+import { MeasurementsMode, MeasurementsRouterParams } from '@/shared/types';
 
 import { ModeDefault } from './mode-default';
 import { ModeCharts } from './mode-charts';
 import { ModeTables } from './mode-tables';
 import { ModeSplit } from './mode-split';
 import { ModeOverlay } from './mode-overlay';
+import { useResultSelectCharts } from '../../hooks';
+import {
+	SingleMeasurementChart,
+	useGetSingleMeasurementQuery
+} from '@/services/bublik-api';
+import { getColorByIdx, SelectedChartsPopover } from '@/shared/charts';
+import { useNavigate, useParams } from 'react-router';
+import { skipToken } from '@reduxjs/toolkit/query';
+import { useSearchParams } from 'react-router-dom';
+
+const modeMap = {
+	[MeasurementsMode.Charts]: ModeCharts,
+	[MeasurementsMode.Split]: ModeSplit,
+	[MeasurementsMode.Tables]: ModeTables,
+	[MeasurementsMode.Overlay]: ModeOverlay,
+	[MeasurementsMode.Default]: ModeDefault
+};
 
 export interface ModePickerProps {
 	mode?: MeasurementsMode;
 }
 
 export const ModePicker = ({ mode }: ModePickerProps) => {
-	switch (mode) {
-		case MeasurementsMode.Charts:
-			return <ModeCharts />;
-		case MeasurementsMode.Split:
-			return <ModeSplit />;
-		case MeasurementsMode.Tables:
-			return <ModeTables />;
-		case MeasurementsMode.Overlay:
-			return <ModeOverlay />;
-		case MeasurementsMode.Default:
-			return <ModeDefault />;
-		default:
-			return <ModeDefault />;
-	}
+	const Component = modeMap[mode ?? MeasurementsMode.Default];
+
+	return (
+		<>
+			<Component />
+			<SelectedChartsContainer />
+		</>
+	);
 };
+
+function SelectedChartsContainer() {
+	const { resultId } = useParams<MeasurementsRouterParams>();
+	const { selectedCharts, resetCharts, removeChart, handleOpenButtonClick } =
+		useResultSelectCharts();
+	const { data } = useGetSingleMeasurementQuery(resultId ?? skipToken);
+
+	return (
+		<SelectedChartsPopover
+			open={!!selectedCharts.length}
+			label="Combined"
+			plots={
+				data?.charts
+					?.filter((chart) => selectedCharts.includes(chart.id))
+					?.map((chart, idx) => ({
+						plot: chart,
+						color: getColorByIdx(idx)
+					})) ?? []
+			}
+			onResetButtonClick={resetCharts}
+			onRemoveClick={(plot) => removeChart(plot.id)}
+			onOpenButtonClick={handleOpenButtonClick}
+		/>
+	);
+}
---
+++ libs/bublik/features/measurements/src/lib/hooks/index.ts
@@ -0,0 +1,58 @@
+import { useNavigate } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
+import {
+	useQueryParam,
+	NumericArrayParam,
+	withDefault
+} from 'use-query-params';
+
+import { SingleMeasurementChart } from '@/services/bublik-api';
+import { MeasurementsMode } from '@/shared/types';
+
+const SELECTED_CHARTS_KEY = 'selectedCharts';
+
+function useResultSelectCharts() {
+	const navigate = useNavigate();
+	const [searchParams] = useSearchParams();
+	const [selectedCharts = [], setSelectedCharts] = useQueryParam(
+		SELECTED_CHARTS_KEY,
+		withDefault(NumericArrayParam, [])
+	);
+
+	const handleOpenButtonClick = () => {
+		const nextParams = new URLSearchParams(searchParams);
+		nextParams.set('mode', MeasurementsMode.Overlay);
+		navigate({ search: nextParams.toString() });
+	};
+
+	function addChart(chartId: number) {
+		setSelectedCharts((prev) => [...(prev || []), chartId]);
+	}
+
+	function removeChart(chartId: number) {
+		setSelectedCharts((prev) => (prev || []).filter((id) => id !== chartId));
+	}
+
+	function resetCharts() {
+		setSelectedCharts([]);
+	}
+
+	function handleChartClick(plot: SingleMeasurementChart) {
+		if (selectedCharts.includes(plot.id)) {
+			removeChart(plot.id);
+		} else {
+			addChart(plot.id);
+		}
+	}
+
+	return {
+		selectedCharts,
+		addChart,
+		removeChart,
+		resetCharts,
+		handleChartClick,
+		handleOpenButtonClick
+	};
+}
+
+export { useResultSelectCharts };
