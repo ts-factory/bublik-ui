58f7f49d592ecc8d01586c549d84cc58b3c7f4b7
Danil Kostromin <danil.kostromin@icloud.com>
2025-05-13 11:35:53 +0300

    style(sidebar): change style of project select to accordion menu
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/projects/src/project-dropdown/project-dropdown.container.tsx
+++ libs/bublik/features/projects/src/project-dropdown/project-dropdown.container.tsx
@@ -1,20 +1,9 @@
 import { useState } from 'react';
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
-import { useQueryParam, NumericArrayParam } from 'use-query-params';
 
 import { bublikAPI } from '@/services/bublik-api';
-import {
-	DropdownMenuContent,
-	DropdownMenuLabel,
-	DropdownMenuPortal,
-	DropdownMenuRadioGroup,
-	DropdownMenuRadioItem,
-	Icon,
-	Separator,
-	Tooltip,
-	cn,
-	useSidebar
-} from '@/shared/tailwind-ui';
+import { Icon, Tooltip, cn, useSidebar } from '@/shared/tailwind-ui';
+
+import { useProjectSearch } from '../hooks';
 
 function ProjectPickerContainer() {
 	const { isSidebarOpen } = useSidebar();
@@ -25,113 +14,115 @@ function ProjectPickerContainer() {
 	} = bublikAPI.useGetAllProjectsQuery();
 	const [isOpen, setIsOpen] = useState(false);
 
-	const [selectedProjectIds = [], setSelectedProjectIds] = useQueryParam(
-		'project',
-		NumericArrayParam
-	);
+	const { projectIds, setProjectsIds } = useProjectSearch();
 
-	const handleValueChange = (projectId: string) => {
-		if (projectId === '') {
-			setSelectedProjectIds([]);
+	const handleValueChange = (projectId: number | undefined) => {
+		if (typeof projectId === 'undefined') {
+			setProjectsIds([]);
 		} else {
-			setSelectedProjectIds([parseInt(projectId, 10)]);
+			setProjectsIds([projectId]);
 		}
 	};
 
 	if (isLoading) return null;
+
 	if (error) return null;
+
 	if (!projects?.length) return null;
 
-	const currentValue =
-		selectedProjectIds?.length === 1 ? selectedProjectIds[0]?.toString() : '';
-
 	return (
-		<DropdownMenu.Root
-			open={isOpen}
-			onOpenChange={(open) => {
-				if (!open) return;
-				setIsOpen(open);
-			}}
+		<div
+			className={cn(
+				'transition-all rounded-lg delay-500 duration-500',
+				isOpen && 'bg-primary-wash'
+			)}
 		>
 			<Tooltip content="Projects" side="right" sideOffset={14}>
-				<DropdownMenu.Trigger asChild>
-					<div
-						className={cn(
-							'group relative rounded-[0.625rem] py-[7px] cursor-pointer',
-							'hover:bg-primary-wash text-text-menu hover:text-primary',
-							'flex min-w-0 flex-grow gap-3.5 items-center',
-							'transition-[padding] duration-500',
-							isSidebarOpen ? 'pl-4' : 'pl-2',
-							isOpen && 'bg-primary text-white'
-						)}
-					>
+				<div
+					className={cn(
+						'group w-full relative rounded-[0.625rem] py-[7px] cursor-pointer',
+						'hover:bg-primary-wash text-text-menu hover:text-primary',
+						'flex min-w-0 flex-grow gap-3.5 items-center',
+						'transition-[padding] duration-500',
+						isSidebarOpen ? 'pl-4' : 'pl-2',
+						isOpen && 'bg-primary hover:bg-primary hover:text-white text-white'
+					)}
+					onClick={() => setIsOpen((v) => !v)}
+				>
+					<div className="flex items-center gap-2">
 						<div className="grid flex-shrink-0 place-items-center">
 							<Icon name="Folder" size={28} />
 						</div>
 						<span className="text-[1.125rem] truncate">Projects</span>
 					</div>
-				</DropdownMenu.Trigger>
-			</Tooltip>
-			<DropdownMenuPortal>
-				<DropdownMenuContent
-					className={cn(
-						'min-w-[238px] rounded-lg bg-white p-1 shadow-popover z-40',
-						'rdx-state-open:rdx-side-top:animate-slide-down-fade',
-						'rdx-state-open:rdx-side-right:animate-slide-left-fade',
-						'rdx-state-open:rdx-side-bottom:animate-slide-up-fade',
-						'rdx-state-open:rdx-side-left:animate-slide-right-fade',
-						'rdx-state-closed:rdx-side-top:animate-fade-out',
-						'rdx-state-closed:rdx-side-right:animate-fade-out',
-						'rdx-state-closed:rdx-side-bottom:animate-fade-out',
-						'rdx-state-closed:rdx-side-left:animate-fade-out'
-					)}
-					sideOffset={0}
-					onInteractOutside={() => setIsOpen(false)}
-					collisionPadding={8}
-				>
-					<DropdownMenuLabel className="text-md">Projects</DropdownMenuLabel>
-					<Separator className="h-px my-1" />
-					<DropdownMenuRadioGroup
-						value={currentValue}
-						onValueChange={handleValueChange}
-						className="gap-1 flex flex-col"
+					<button
+						aria-label="Toggle submenu"
+						className={cn(
+							'grid place-items-center ml-auto pr-4 rounded flex-shrink-0 [&_svg]:transition-transform [&_svg]:rounded',
+							isOpen
+								? '[&_svg]:rotate-180 [&_svg]:hover:bg-primary-wash [&_svg]:hover:text-primary'
+								: '[&_svg]:rotate-90 [&_svg]:hover:bg-primary-wash'
+						)}
 					>
-						<DropdownMenuRadioItem
-							value=""
+						<Icon name="ArrowShortTop" size={24} className="" />
+					</button>
+				</div>
+			</Tooltip>
+			<div
+				className={cn(
+					'[&>ul]:overflow-hidden grid transition-all transform-gpu ease-in-out motion-reduce:transition-none duration-500',
+					isOpen ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'
+				)}
+			>
+				<ul className="gap-1 flex flex-col mt-3.5">
+					<li
+						onClick={() => handleValueChange(undefined)}
+						role="button"
+						className={cn(
+							'flex items-center rounded-[10px] py-1.5 h-full gap-3.5 group pl-8 pr-[18px] text-text-menu hover:text-primary group',
+							!projectIds?.length &&
+								'bg-[#ecf1ff] text-[#385bf9] focus:bg-[#ecf1ff] focus:text-[#385bf9]'
+						)}
+					>
+						<div
 							className={cn(
-								!selectedProjectIds?.length &&
-									'bg-[#ecf1ff] text-[#385bf9] focus:bg-[#ecf1ff] focus:text-[#385bf9]'
+								'w-2 h-2 rounded-full bg-primary',
+								projectIds?.length && 'opacity-0'
 							)}
-						>
-							<span className="truncate text-md">All</span>
-						</DropdownMenuRadioItem>
-						{projects.map((project) => {
-							const isSelected = selectedProjectIds?.includes(project.id);
+						/>
+						<span className="truncate text-[0.875rem] leading-[1.5rem]">
+							All
+						</span>
+					</li>
+					{projects.map((project) => {
+						const isSelected = projectIds?.includes(project.id);
 
-							return (
-								<DropdownMenuRadioItem
-									key={project.id}
-									value={project.id.toString()}
+						return (
+							<li
+								key={project.id}
+								role="button"
+								onClick={() => handleValueChange(project.id)}
+								className={cn(
+									'flex items-center rounded-[10px] py-1.5 h-full gap-3.5 group pl-8 pr-[18px] text-text-menu hover:text-primary group',
+									isSelected &&
+										'bg-[#ecf1ff] text-[#385bf9] focus:bg-[#ecf1ff] focus:text-[#385bf9]'
+								)}
+							>
+								<div
 									className={cn(
-										isSelected &&
-											'bg-[#ecf1ff] text-[#385bf9] focus:bg-[#ecf1ff] focus:text-[#385bf9]'
+										'w-2 h-2 rounded-full bg-primary',
+										!isSelected && 'opacity-0'
 									)}
-								>
-									<span className="truncate text-md">{project.name}</span>
-								</DropdownMenuRadioItem>
-							);
-						})}
-					</DropdownMenuRadioGroup>
-					{/* <DropdownMenuSeparator /> */}
-					{/* <CreateProjectModal>
-						<DropdownMenuItem className="pl-2">
-							<Icon name="FilePlus" size={20} className="mr-2" />
-							<span className="text-md">New Project</span>
-						</DropdownMenuItem>
-					</CreateProjectModal> */}
-				</DropdownMenuContent>
-			</DropdownMenuPortal>
-		</DropdownMenu.Root>
+								/>
+								<span className="truncate text-[0.875rem] leading-[1.5rem]">
+									{project.name}
+								</span>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</div>
 	);
 }
 
