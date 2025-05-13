28e3e3eee13602e518f0a9c67e7a06ef21efc921
Danil Kostromin <danil.kostromin@icloud.com>
2025-05-13 14:00:34 +0300

    fix(history): missing `project` search param on history submit
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/projects/src/index.ts
+++ libs/bublik/features/projects/src/index.ts
@@ -2,4 +2,5 @@ export * from './project-dropdown';
 export * from './create-project-modal';
 export * from './update-project-modal';
 export * from './link-with-project';
+export * from './constants';
 export * from './hooks';
