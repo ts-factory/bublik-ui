630071d0a7884b5ad4e639cf14a9dfa6d1f4faf4
Danil Kostromin <danil.kostromin@icloud.com>
2025-05-21 01:58:13 +0300

    style(result): [measurement] fix selection popover overlap with table header
    This is cause by missing specifier for z-index on the popover
    So table header is layed out on top of the popover
    
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/shared/charts/src/lib/selected-charts/selected-charts.component.tsx
+++ libs/shared/charts/src/lib/selected-charts/selected-charts.component.tsx
@@ -35,7 +35,7 @@ function SelectedChartsPopover(props: SelectedChartsPopoverProps) {
 					animate="visible"
 					exit="exit"
 					transition={transition}
-					className="fixed bottom-4 right-4 bg-white rounded-lg shadow-popover min-w-[360px] px-4 py-2"
+					className="fixed bottom-4 right-4 bg-white rounded-lg shadow-popover min-w-[360px] px-4 py-2 z-30"
 				>
 					<h1 className="text-[0.875rem] leading-[1.125rem] font-semibold mb-2">
 						{props.label}
