e2a50a16c50b82ddaea2da9b453ac0cefd44f0c4
Danil Kostromin <danil.kostromin@icloud.com>
2025-07-23 02:24:18 +0300

    feat(report): [TOC] remember open/close state in URL
    When user opens or closes TOC item we should remember state in URL
    So users can share and save URL's with state
    
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/run-report/src/lib/run-report.component.tsx
+++ libs/bublik/features/run-report/src/lib/run-report.component.tsx
@@ -1,6 +1,15 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2024 OKTET LTD */
 import { ComponentProps, useCallback, useMemo, useRef, useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import { BooleanParam, useQueryParam, withDefault } from 'use-query-params';
+import { useLocation } from 'react-router';
+import {
+	createColumnHelper,
+	flexRender,
+	getCoreRowModel,
+	useReactTable
+} from '@tanstack/react-table';
 
 import { useMount } from '@/shared/hooks';
 import {
@@ -15,24 +24,16 @@ import {
 	Spinner,
 	toast
 } from '@/shared/tailwind-ui';
-
 import {
 	NotProcessedPoint,
 	ReportRoot,
 	RunDetailsAPIResponse,
 	TestBlock
 } from '@/shared/types';
+import { getErrorMessage } from '@/services/bublik-api';
+
 import { List, RunReportHeader } from './run-report-header';
 import { RunReportTestBlock, WarningsHoverCard } from './run-report-test';
-import { useLocation } from 'react-router';
-import { getErrorMessage } from '@/services/bublik-api';
-import {
-	createColumnHelper,
-	flexRender,
-	getCoreRowModel,
-	useReactTable
-} from '@tanstack/react-table';
-import { Link, useSearchParams } from 'react-router-dom';
 
 interface TableOfContentsItem {
 	type: string;
@@ -93,7 +94,10 @@ interface TableOfContentsItemProps {
 function TableOfContentsItem({ item, depth = 0 }: TableOfContentsItemProps) {
 	const isOpenByDefault =
 		item.type === 'arg-val-block' || item.type === 'test-block';
-	const [open, setOpen] = useState(isOpenByDefault);
+	const [open, setOpen] = useQueryParam(
+		item.id,
+		withDefault(BooleanParam, isOpenByDefault)
+	);
 	const [params] = useSearchParams();
 	const configid = params.get('config');
 
