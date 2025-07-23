14dcf1773db9113c696a1e50e27bf69e609a85c8
Danil Kostromin <danil.kostromin@icloud.com>
2025-07-23 05:32:46 +0300

    refactor(run): [results] use split button to display links to history
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/result-links/src/lib/link-to-history/link-to-history.tsx
+++ libs/bublik/features/result-links/src/lib/link-to-history/link-to-history.tsx
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
-import { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
 import { getHistorySearch } from '@/shared/utils';
@@ -9,7 +8,7 @@ import {
 	HistoryMode,
 	RunDataResults
 } from '@/shared/types';
-import { ContextLinks, ContextLinksSection, Icon } from '@/shared/tailwind-ui';
+import { Icon, SplitButton, Tooltip } from '@/shared/tailwind-ui';
 
 export interface LinkToHistoryProps {
 	result: RunDataResults;
@@ -22,62 +21,102 @@ export const LinkToHistory = (props: LinkToHistoryProps) => {
 	const search = getHistorySearch(runDetails, result, userPreferredHistoryMode);
 	const prefilled = { pathname: '/history', search: search.prefilled };
 
-	const sections = useMemo<ContextLinksSection[]>(
-		() => [
-			{
-				label: 'Prefilled',
-				items: [
-					{
-						label: 'By all possible parameters',
-						to: { pathname: '/history', search: search.prefilled },
-						state: { fromRun: true }
-					}
-				]
-			},
-			{
-				label: 'Direct',
-				items: [
-					{
-						label: 'By test name',
-						to: { pathname: '/history', search: search.byTestName }
-					},
-					{
-						label: 'By iteration',
-						to: { pathname: '/history', search: search.byIteration }
-					},
-					{
-						label: 'By iteration and important tags',
-						to: {
-							pathname: '/history',
-							search: search.byIterationWithImportant
-						}
-					},
-					{
-						label: 'By iteration and all tags',
-						to: { pathname: '/history', search: search.byIterationWithAllTags }
-					}
-				]
-			}
-		],
-		[
-			search.byIteration,
-			search.byIterationWithAllTags,
-			search.byIterationWithImportant,
-			search.byTestName,
-			search.prefilled
-		]
-	);
-
 	return (
-		<ContextLinks sections={sections}>
-			<Link
-				className="flex items-center w-full gap-1"
-				to={prefilled}
-				state={{ fromRun: true }}
-			>
-				<Icon name="BoxArrowRight" className="grid place-items-center" />
-				History
-			</Link>
-		</ContextLinks>
+		<SplitButton.Root variant="secondary" size="xss">
+			<SplitButton.Button asChild>
+				<Link to={prefilled} state={{ fromRun: true }} className="">
+					<Icon
+						name="BoxArrowRight"
+						size={20}
+						className="grid place-items-center mr-1"
+					/>
+					<span>History</span>
+				</Link>
+			</SplitButton.Button>
+			<SplitButton.Separator orientation="vertical" className="h-5" />
+			<SplitButton.Trigger>
+				<Icon name="ChevronDown" size={14} />
+			</SplitButton.Trigger>
+			<SplitButton.Content align="start">
+				<SplitButton.Label>Prefilled</SplitButton.Label>
+				<SplitButton.Separator className="my-1" />
+				<Tooltip
+					content="View history with all tags, test parameters, and verdicts prefilled"
+					side="right"
+					sideOffset={8}
+				>
+					<SplitButton.Item asChild>
+						<Link
+							to={{ pathname: '/history', search: search.prefilled }}
+							state={{ fromRun: true }}
+						>
+							<Icon name="BoxArrowRight" size={20} className="text-primary" />
+							<span>All Parameters</span>
+						</Link>
+					</SplitButton.Item>
+				</Tooltip>
+				<SplitButton.Separator className="my-1" />
+				<SplitButton.Label>Direct</SplitButton.Label>
+				<SplitButton.Separator className="my-1" />
+				<Tooltip
+					content="View history for this test name with all default settings"
+					side="right"
+					sideOffset={8}
+				>
+					<SplitButton.Item asChild>
+						<Link to={{ pathname: '/history', search: search.byTestName }}>
+							<Icon name="BoxArrowRight" size={20} className="text-primary" />
+							<span>Test Name</span>
+						</Link>
+					</SplitButton.Item>
+				</Tooltip>
+				<Tooltip
+					content="View history for this test name with parameters"
+					side="right"
+					sideOffset={8}
+				>
+					<SplitButton.Item asChild>
+						<Link to={{ pathname: '/history', search: search.byIteration }}>
+							<Icon name="BoxArrowRight" size={20} className="text-primary" />
+							<span>Iteration</span>
+						</Link>
+					</SplitButton.Item>
+				</Tooltip>
+				<Tooltip
+					content="View history for this test with important tags and parameters"
+					side="right"
+					sideOffset={8}
+				>
+					<SplitButton.Item asChild>
+						<Link
+							to={{
+								pathname: '/history',
+								search: search.byIterationWithImportant
+							}}
+						>
+							<Icon name="BoxArrowRight" size={20} className="text-primary" />
+							<span>Iteration And Important</span>
+						</Link>
+					</SplitButton.Item>
+				</Tooltip>
+				<Tooltip
+					content="View history for this test with all tags and parameters"
+					side="right"
+					sideOffset={8}
+				>
+					<SplitButton.Item asChild>
+						<Link
+							to={{
+								pathname: '/history',
+								search: search.byIterationWithAllTags
+							}}
+						>
+							<Icon name="BoxArrowRight" size={20} className="text-primary" />
+							<span>Iteration And Tags</span>
+						</Link>
+					</SplitButton.Item>
+				</Tooltip>
+			</SplitButton.Content>
+		</SplitButton.Root>
 	);
 };
