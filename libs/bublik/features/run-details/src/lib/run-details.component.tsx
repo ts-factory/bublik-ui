e8c659837b41e8d6e6eaa8e1b4cd27b932d3c3c2
Danil Kostromin <danil.kostromin@icloud.com>
2026-01-30 04:35:09 +0300

    chore(run): [details] remove unused import
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/run-details/src/lib/run-details.component.tsx
+++ libs/bublik/features/run-details/src/lib/run-details.component.tsx
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /* SPDX-FileCopyrightText: 2021-2023 OKTET Labs Ltd. */
-import { FC, useMemo } from 'react';
+import { useMemo } from 'react';
 
 import { DetailsItem, RUN_STATUS } from '@/shared/types';
 import { parseDetailDate, trimBranch } from '@/shared/utils';
