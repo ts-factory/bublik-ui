0ec689529a5809cbb9507d3f4f9cd34279029aea
Danil Kostromin <danil.kostromin@icloud.com>
2026-02-17 00:36:51 +0300

    feat(sidebar): add sidebar primitive components
    Add sidebar primitive components for construction of sidebar items.
    This will allow different features to build custom sidebar items from
    primitives with custom logic like displaying loading indicators,
    dynamically enable/disable links etc.
    
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

---
+++ libs/bublik/features/sidebar-nav/src/lib/sidebar-nav.utils.ts
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* SPDX-FileCopyrightText: 2024-2026 OKTET LTD */
+import type { To } from 'react-router-dom';
+
+export function toString(to: To): string {
+	if (typeof to === 'string') return to;
+	return `${to.pathname}${to.search ? `?${to.search}` : ''}${
+		to.hash ? `#${to.hash}` : ''
+	}`;
+}
